4b42d6e42d796368b13d0089be27faaad2e545f6
Uday Lal <udaylal014@gmail.com>
2025-07-10 22:31:00 +0530

    add diagnotic keybinding and remove hologram

--- lua/tiger/keymap.lua
+++ lua/tiger/keymap.lua
@@ -3,9 +3,15 @@ local keymap = vim.keymap
 vim.g.mapleader = " "
 
 function open_window_right()
-	print("opening window to the right")
-	vim.cmd("botright vs")
+    print("opening window to the right")
+    vim.cmd("botright vs")
+end
+
+function open_diagnostics()
+    print("Opening Diagnostics")
+    vim.diagnostic.open_float()
 end
 
 keymap.set("n", "<C-f>", vim.cmd.NvimTreeToggle)
 keymap.set("n", "<leader>sw", open_window_right, { noremap = true, silent = true })
+keymap.set("n", "<leader>od", open_diagnostics)
