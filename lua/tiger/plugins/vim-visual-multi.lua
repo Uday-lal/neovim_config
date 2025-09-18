0e9c1f082f877af532e53365d58b295792e0b554
Uday Lal <uday.lal@mrndigital.in>
2025-09-18 12:24:27 +0530

    add plugins for multi cursor editing

---
+++ lua/tiger/plugins/vim-visual-multi.lua
@@ -0,0 +1,9 @@
+return {
+	"mg979/vim-visual-multi",
+	branch = "master", -- important, plugin uses a specific branch
+	init = function()
+		-- optional: set your own keymaps/settings here
+		-- Example: let g:VM_default_mappings = 0
+		-- vim.g.VM_maps = { ["Find Under"] = "<C-d>" }
+	end,
+}
